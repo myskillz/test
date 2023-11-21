f300d159a3f6928621be903031f5be03ff59e8bb
Super Q <qq@SuperQ-swif5>
2023-11-21 20:53:07 +0000

    initial

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+<div>adsfasdf</div>
+dfasdfs
+props 
+{{ event }}
+</template>
+
+
+
+
+<script setup>
+import { ref } from 'vue';
+
+import axios from 'axios';
+
+const props = defineProps({
+    event: String,
+});
+</script>
